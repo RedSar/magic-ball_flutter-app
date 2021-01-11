df1107440f2653fa3b26a487b12c2a5e43d94c2f
Redouane Guerchal <redouane.devpurpose@gmail.com>
2021-01-11 12:57:58 +0100

    App finished!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+            backgroundColor: Colors.blue,
+            appBar: AppBar(
+                backgroundColor: Colors.blue.shade800,
+                title: Center(
+                  child: Text(
+                    'Ask Me Anything!',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                )),
+            body: Center(child: MagicBall())),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int answerNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            answerNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$answerNumber.png'),
+      ),
+    );
+  }
+}
